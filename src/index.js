9e8bf2262c3892db9433caf717895381ff8d8bec
sumdude65 <imadeosaretin@gmail.com>
2021-02-18 08:42:45 +0100

    new project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Faq } from "./components/Faq/index";
-import { Header } from "./components/Title/index";
 import "./style.css";
-
-const App = () => {
-  return (
-    <main className="main">
-      <Header />
-      <Faq />
-    </main>
-  );
-};
-ReactDOM.render(<App />, document.getElementById("root"));
