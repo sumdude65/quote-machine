1298a6f217599349b29563b712c841af36d87a51
sumdude65 <imadeosaretin@gmail.com>
2021-02-18 16:53:25 +0100

    Added Carousel functionality
    still buggy

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,17 +5,67 @@ import { Review } from "../review/index";
 
 export const App = () => {
   const entries = [...Data];
-  let counter = 0;
-  const [currentReview, setCurrentReview] = useState(entries[0]);
+  const [currentReview, setCurrentReview] = useState(0);
+  const updateReview = () => {
+    if (currentReview === entries.length - 1) {
+      setCurrentReview(0);
+    } else {
+      setCurrentReview(currentReview + 1);
+    }
+  };
   useEffect(() => {
-    setTimeout(() => {
-      counter++;
-      setCurrentReview(entries[counter]);
-    }, 2000);
-  }, [currentReview]);
+    setTimeout(updateReview, 3000);
+    return () => {
+      clearTimeout(updateReview, 3000);
+    };
+  });
+  const previous = () => {
+    if (currentReview === 0) {
+      setCurrentReview(entries.length - 1);
+    } else {
+      setCurrentReview(currentReview - 1);
+    }
+  };
+  const next = () => {
+    if (currentReview === entries.length - 1) {
+      setCurrentReview(0);
+    } else {
+      setCurrentReview(currentReview + 1);
+    }
+  };
   return (
     <main className="main">
-      <Review props={currentReview} />
+      <button className="btn left" onClick={previous}>
+        <svg
+          stroke="currentColor"
+          fill="none"
+          stroke-width="2"
+          viewBox="0 0 24 24"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          height="1em"
+          width="1em"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <polyline points="15 18 9 12 15 6"></polyline>
+        </svg>
+      </button>
+      <Review props={entries[currentReview]} />
+      <button className="btn right" onClick={next}>
+        <svg
+          stroke="currentColor"
+          fill="none"
+          stroke-width="2"
+          viewBox="0 0 24 24"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          height="1em"
+          width="1em"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <polyline points="9 18 15 12 9 6"></polyline>
+        </svg>
+      </button>
     </main>
   );
 };
